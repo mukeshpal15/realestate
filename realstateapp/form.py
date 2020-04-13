b447969eebccb332445cc28a39830619263d3404
mukesh pal <mukeshpalmp015@gmail.com>
2020-04-13 17:31:28 +0530

    fourthteen commit

--- realstateapp/form.py
+++ realstateapp/form.py
@@ -1,6 +1,6 @@
-'''from django import forms
+from django import forms
 
 class ImageUploadForm(forms.Form):
 	image = forms.ImageField()
 class upload_image(forms.Form):
-	img=forms.ImageField() '''
+	img=forms.ImageField()
